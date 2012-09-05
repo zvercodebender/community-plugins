cda662fffc6d91d814e804af3c8529f75b775ab0
Martin van Vliet <mvanvliet@xebia.com>
2012-09-05 14:25:21 +0200

    Made community plugins compile with Deployit 3.8 alpha release

--- deployit-udm-plugins/utility-plugins/precheck-plugin/src/main/java/ext/deployit/community/plugin/precheck/planning/PrecheckStepsGenerator.java
+++ deployit-udm-plugins/utility-plugins/precheck-plugin/src/main/java/ext/deployit/community/plugin/precheck/planning/PrecheckStepsGenerator.java
@@ -1,9 +1,5 @@
 package ext.deployit.community.plugin.precheck.planning;
 
-import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Iterables.transform;
-import static com.xebialabs.deployit.plugin.api.deployment.specification.Operation.DESTROY;
-
 import java.util.List;
 import java.util.Map;
 
@@ -15,15 +11,20 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.xebialabs.deployit.plugin.api.deployment.execution.DeploymentStep;
+
 import com.xebialabs.deployit.plugin.api.deployment.planning.PrePlanProcessor;
 import com.xebialabs.deployit.plugin.api.deployment.specification.Delta;
 import com.xebialabs.deployit.plugin.api.deployment.specification.DeltaSpecification;
+import com.xebialabs.deployit.plugin.api.flow.Step;
 import com.xebialabs.deployit.plugin.api.udm.Container;
 import com.xebialabs.deployit.plugin.generic.step.ScriptExecutionStep;
 import com.xebialabs.deployit.plugin.overthere.HostContainer;
 
 import ext.deployit.community.plugin.precheck.util.Strings;
+
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.transform;
+import static com.xebialabs.deployit.plugin.api.deployment.specification.Operation.DESTROY;
 
 
 public class PrecheckStepsGenerator {
@@ -47,7 +48,7 @@ public class PrecheckStepsGenerator {
 	};
 
 	@PrePlanProcessor
-	public static List<DeploymentStep> generate(DeltaSpecification specification) {
+	public static List<Step> generate(DeltaSpecification specification) {
 		Map<String, Object> freeMarkerContext = Maps.newHashMap();
 		freeMarkerContext.put("deployedApplication", specification.getDeployedApplication());
 
@@ -55,7 +56,7 @@ public class PrecheckStepsGenerator {
 				transform(specification.getDeltas(), TO_CONTAINER),
 				IS_PRECHECKED_CONTAINER));
 
-		ImmutableList.Builder<DeploymentStep> steps = ImmutableList.builder();
+		ImmutableList.Builder<Step> steps = ImmutableList.builder();
 		for (Container container : preCheckedContainers) {
 			freeMarkerContext.put("container", container);
 			if (container instanceof HostContainer) {
