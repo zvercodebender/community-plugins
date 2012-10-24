11644feeb94ca2a957c0a489ef2cea0c3b497c5e
Benoit Moussaud <bmoussaud@xebialabs.com>
2012-10-24 19:58:37 +0200

    simple scheduler plugin (migration to 3.8)
    Conflicts:
    
    	deployit-udm-plugins/utility-plugins/pom.xml

---
+++ deployit-udm-plugins/utility-plugins/scheduler-plugin/README.md
@@ -0,0 +1,17 @@
+# Scheduler plugin #
+
+# Overview #
+
+The scheduler plugin allows to schedule the execution of the remaining steps after a given date.
+
+# Requirements #
+
+* **Deployit requirements**
+	* **Deployit**: version 3.8
+
+# Installation #
+
+Place the plugin JAR file into your `SERVER_HOME/plugins` directory. 
+
+The date and the hours are defined as transient at the udm.DeployedApplication level.
+The format used to defined the date and the time are configurable using the datePattern and timePattern hidden properties.
---
+++ deployit-udm-plugins/utility-plugins/scheduler-plugin/src/main/java/ext/deployit/community/plugin/scheduler/contributor/SchedulerContributor.java
@@ -0,0 +1,50 @@
+package ext.deployit.community.plugin.scheduler.contributor;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+
+import com.xebialabs.deployit.plugin.api.deployment.planning.PrePlanProcessor;
+import com.xebialabs.deployit.plugin.api.deployment.specification.DeltaSpecification;
+import com.xebialabs.deployit.plugin.api.flow.Step;
+import com.xebialabs.deployit.plugin.api.udm.DeployedApplication;
+
+import ext.deployit.community.plugin.scheduler.step.AtDateWaitStep;
+
+import static java.lang.String.format;
+
+public class SchedulerContributor {
+
+    @PrePlanProcessor
+    public Step injectPersonalCredentials(DeltaSpecification specification) {
+        final DeployedApplication deployedApplication = specification.getDeployedApplication();
+
+        final String date = deployedApplication.getProperty("date");
+        final String time = deployedApplication.getProperty("time");
+        final String datePattern = deployedApplication.getProperty("datePattern");
+        final String timePattern = deployedApplication.getProperty("timePattern");
+
+        if (Strings.isNullOrEmpty(date) && Strings.isNullOrEmpty(time)) {
+            return null;
+        }
+        if (Strings.isNullOrEmpty(date) || Strings.isNullOrEmpty(time)) {
+            logger.warn("date {} or time {} is empty", date, time);
+            return null;
+        }
+
+        final DateFormat dateFormat = new SimpleDateFormat(format("%s %s", datePattern, timePattern));
+        try {
+            final Date parsed = dateFormat.parse(format("%s %s", date, time));
+            return new AtDateWaitStep(parsed);
+        } catch (ParseException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    protected static final Logger logger = LoggerFactory.getLogger(SchedulerContributor.class);
+}
