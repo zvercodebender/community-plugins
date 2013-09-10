c3b4e13b9a976bba9af9abd5711c1b2f795f4f8c
Martin van Vliet <mvanvliet@xebialabs.com>
2013-09-10 14:43:04 +0200

    DEPLOYITPB-4029 renamed variable 'type' to 'ciType' to avoid collision with built-in python type

--- tool-deployit-plugins/deployit-puppet-module/files/create-ci.py
+++ tool-deployit-plugins/deployit-puppet-module/files/create-ci.py
@@ -4,7 +4,7 @@ import sys, re
 print sys.argv
 
 id = sys.argv[1]
-type = sys.argv[2]
+ciType = sys.argv[2]
 
 values = {}
 for i in range(3, len(sys.argv)):
@@ -13,13 +13,13 @@ for i in range(3, len(sys.argv)):
 	if match != None:
 		values[match.group(1)] = match.group(2)
 
-print "Creating CI with id ",id," and type", type, " values ", values
+print "Creating CI '%s' of type '%s' with values: %s" % (id, ciType, values)
 
 try:
 	ci = repository.read(id)
 
 except:
-	ci = factory.configurationItem(id, type, values)
+	ci = factory.configurationItem(id, ciType, values)
 	print "Created CI:", ci
 
 	repository.create(ci)
