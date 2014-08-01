547529a7eb3a09562ccba8238e2a6203fe17a8e9
Vanya Stanislavciuc <istanislavciuc@xebialabs.com>
2014-08-01 15:06:24 +0200

    Fixing rules example plugin

--- example-plugins/rule-demo-plugin/plugin-resources/planning/start-stop-server.py
+++ example-plugins/rule-demo-plugin/plugin-resources/planning/start-stop-server.py
@@ -1,17 +1,27 @@
 from java.util import HashSet
 
-def getContainers():
-	containers = HashSet()
-	for _delta in deltas.deltas:
-		deployed = _delta.deployedOrPrevious
-		currentContainer = deployed.container
-		if (_delta.operation != "NOOP" and currentContainer.type == "example.Server"):
-			containers.add(currentContainer)
-	return containers
+def containers():
+    result = HashSet()
+    for _delta in deltas.deltas:
+        deployed = _delta.deployedOrPrevious
+        current_container = deployed.container
+        if _delta.operation != "NOOP" and current_container.type == "example.Server":
+            result.add(current_container)
+    return result
 
 
-for container in getContainers():
-	stepVars = {'container': container}
-	context.addStep(steps.execute_remote_os_script(description = "Stopping server %s" % container.name, order = 20, script_template_path = "scripts/stop", vars = stepVars, container = container.host))
-	context.addStep(steps.execute_remote_os_script(description = "Starting server %s" % container.name, order = 80, script_template_path = "scripts/start", vars = stepVars, container = container.host))
+for container in containers():
+    context.addStep(steps.os_script(
+        description="Stopping server %s" % container.name,
+        order=20,
+        script="scripts/stop",
+        freemarker_context={'container': container},
+        target_host=container.host)
+    )
+    context.addStep(steps.os_script(
+        description="Starting server %s" % container.name,
+        order=80,
+        script="scripts/start",
+        freemarker_context={'container': container},
+        target_host=container.host))
 
